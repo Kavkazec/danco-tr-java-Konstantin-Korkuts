28de3d7bd7f0a3441787aa89850db1bdf72a9d48
Konstantin <znemozero@mail.ru>
2015-12-13 15:36:14 +0300

    Task1

--- Sheet job3/SOJ3Task1/src/Massiv.java
+++ Sheet job3/SOJ3Task1/src/Massiv.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Massiv {
-	String[] mass = {"Костя", "Паша", "Макс", "Саша", "Настя", "Юля", "Катя", "Захар"};
+	String[] mass = {"Kostya", "Pasha", "Max", "Sasha", "Nastya", "Julya", "Katya", "Zahar"};
 	void sort(){
 		Arrays.sort(mass);
 		for(int i = 0; i < mass.length; i++){
