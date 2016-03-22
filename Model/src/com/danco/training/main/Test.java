944fb85f4e3ba9b0cca3a5e8c8399a6eea81e81d
Konstantin <znemozero@mail.ru>
2016-03-22 15:29:00 +0300

    11

---
+++ Model/src/com/danco/training/main/Test.java
@@ -0,0 +1,9 @@
+package com.danco.training.main;
+
+import com.danco.training.persistexception.PersistenceException;
+
+public class Test {
+	public static void main(String[] args) throws PersistenceException {
+		
+	}
+}
